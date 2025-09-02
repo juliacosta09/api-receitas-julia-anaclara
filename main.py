abe729be22da0c5939db4977a88432432797b265
Júlia <julia.costa09@aluno.ifce.edu.br>
2025-09-02 11:13:53 +0000

    alteração

---
+++ main.py
@@ -0,0 +1,20 @@
+receitas = [
+    {
+        "nome": "brownie",
+        "ingredientes": ["3 ovos", "6 colheres de açúcar", "2 xícaras de chocolate em pó"],
+        "utensilios": ["tigela", "forma"],
+        "modo_preparo": "Misture tudo e leve ao forno por 40 minutos."
+    },
+    {
+        "nome": "torta",
+        "ingredientes": ["3 ovos", "1 xícara de leite", "2 xícaras de farinha"],
+        "utensilios": ["liquidificador", "forma"],
+        "modo_preparo": "Bata tudo no liquidificador e asse por 30 minutos."
+    },
+    {
+        "nome": "bolo de cenoura",
+        "ingredientes": ["3 cenouras", "3 ovos", "2 xícaras de açúcar"],
+        "utensilios": ["liquidificador", "forma"],
+        "modo_preparo": "Bata os ingredientes e asse por 40 minutos."
+    },
+]
